88d72e857b7ef61895e31afc66f37862e2444593
USER <user@USER-no-MacBook-Air-2.local>
2018-12-23 14:04:05 +0900

    routesの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
-  get 'talks/index'
-
-  get 'talks/show'
-
-  get 'talks/edit'
-
-  get 'talks/new'
-
-  get 'talks/destroy'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources:talks do
+    collection do
+      post :confirm
+    end
+  end
 end
